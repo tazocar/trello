d3a9677d76c52635c0f29a92e3bd42dd2fd58e66
tazocar <tankodani@gmail.com>
2017-11-08 17:26:25 -0300

    input tarjetas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,12 @@
+var listCont = document.createElement("section");
+listCont.id = "lists-container";
+document.body.appendChild(listCont);
+
+
 //llamo a mi contenedor de TODAS las listas
-var listCont = document.getElementById("lists-container");
+/*var listCont = document.getElementById("lists-container");*/
+
+
 //creo un div contenedor para cada lista
 var inputDiv = document.createElement("div");
 //le doy clase
@@ -10,6 +17,7 @@ var inputStart = document.createElement("input");
 inputStart.setAttribute("type", "text")
 inputStart.setAttribute("placeholder", "Añadir una lista");
 inputStart.classList.add("input-start");
+inputStart.id = "setCardName"
 
 //Creando Botón
 var btnSave = document.createElement("button");
@@ -25,6 +33,61 @@ cancel.classList.add("fa", "fa-times");
 //Uniendo hijos
 inputDiv.appendChild(inputStart);
 listCont.appendChild(inputDiv);
+
+///////////////////////////////////////
+
+//Evento de click para agregar tarjetas
+
+btnSave.addEventListener("click", function(){
+	var listDiv = document.createElement("div");
+	//creo nodo de texto con value del input
+	var titleCard = document.createTextNode(document.getElementById("setCardName").value);
+	if (titleCard.length > 0) {
+		//le agrego clases a mi div
+		listDiv.classList.add("list-div", "list-div-active");
+		//uno los hijos
+		var titleH = document.createElement("h4");
+		titleH.appendChild(titleCard);
+		//crear link
+		var addCardLink = document.createElement("a");
+		addCardLink.setAttribute("href", "#");
+		var textCardLink = document.createTextNode("Añadir una tarjeta");
+		//uno los hijos
+		addCardLink.appendChild(textCardLink);
+		listDiv.appendChild(titleH);
+		listDiv.appendChild(addCardLink);
+		listCont.insertBefore(listDiv, inputDiv);
+		//limpio mi input
+		document.getElementById("setCardName").value = "";
+		//le quito el diseño de focus
+		inputStart.classList.remove('input-focus');
+		inputDiv.className = "list-div"
+		inputDiv.removeChild(btnSave);
+		inputDiv.removeChild(cancel);
+
+		/////////// Evento Input para añadir tarjeta
+		addCardLink.addEventListener('click', function(){
+			var textArea = document.createElement("textarea");
+			var btnSend = document.createElement("button");
+			var btnSendText = document.createTextNode("Añadir")
+			btnSend.appendChild(btnSendText);
+
+
+			listDiv.appendChild(textArea);
+			listDiv.appendChild(btnSend);
+			//remueve a
+			listDiv.removeChild(addCardLink);
+
+		})
+		//////////////
+	}
+});
+
+
+
+///////////////////////////////////////////
+
+///////////////////////////////////////////
 
 //Evento en el input
 inputStart.addEventListener('focus', firstCardFocus);
@@ -43,5 +106,5 @@ function firstCardBlur(){
 	inputDiv.className = "list-div"
 	inputDiv.removeChild(btnSave);
 	inputDiv.removeChild(cancel);
-
+	document.getElementById("setCardName").value = "";
 }
