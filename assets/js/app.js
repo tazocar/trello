96cb0ffc224eef90cb52e1ab5575ea6a09d9c2c7
tazocar <tankodani@gmail.com>
2017-11-08 11:41:56 -0300

    body nowrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 //llamo a mi contenedor de TODAS las listas
 var listCont = document.getElementById("lists-container");
 //creo un div contenedor para cada lista
-var listDiv = document.createElement("div");
+var inputDiv = document.createElement("div");
 //le doy clase
-listDiv.setAttribute("class", "list-div");
+inputDiv.setAttribute("class", "list-div");
 
 //creo un input
 var inputStart = document.createElement("input");
@@ -23,26 +23,25 @@ cancel.classList.add("fa", "fa-times");
 
 
 //Uniendo hijos
-listDiv.appendChild(inputStart);
-listCont.appendChild(listDiv);
+inputDiv.appendChild(inputStart);
+listCont.appendChild(inputDiv);
 
 //Evento en el input
 inputStart.addEventListener('focus', firstCardFocus);
 function firstCardFocus(){
 	//al hacer click en el input
 	inputStart.classList.add('input-focus');
-	listDiv.className = "list-div-active"
-	listDiv.appendChild(btnSave);
-	listDiv.appendChild(cancel);
+	inputDiv.className = "list-div-active"
+	inputDiv.appendChild(btnSave);
+	inputDiv.appendChild(cancel);
 
 }
-
 cancel.addEventListener('click', firstCardBlur);
 function firstCardBlur(){
 	//al hacer click en el input
 	inputStart.classList.remove('input-focus');
-	listDiv.className = "list-div"
-	listDiv.removeChild(btnSave);
-	listDiv.removeChild(cancel);
+	inputDiv.className = "list-div"
+	inputDiv.removeChild(btnSave);
+	inputDiv.removeChild(cancel);
 
 }
