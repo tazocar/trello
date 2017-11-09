7e941e09d3eac0522e6a0329c9b800e592de6c9b
tazocar <tankodani@gmail.com>
2017-11-09 01:05:10 -0300

    falta diseño

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 var listCont = document.createElement("section");
 listCont.id = "lists-container";
-document.body.appendChild(listCont);
+var scriptJs = document.getElementsByTagName("script")[0];
+document.body.insertBefore(listCont, scriptJs);
 
 
 //llamo a mi contenedor de TODAS las listas
@@ -45,7 +46,7 @@ btnSave.addEventListener("click", function(){
 	if (titleCard.length > 0) {
 		//le agrego clases a mi div
 		listDiv.classList.add("list-div", "list-div-active");
-		//uno los hijos
+		//creo titulo
 		var titleH = document.createElement("h4");
 		titleH.appendChild(titleCard);
 		//crear link
@@ -59,50 +60,105 @@ btnSave.addEventListener("click", function(){
 		listCont.insertBefore(listDiv, inputDiv);
 		//limpio mi input
 		document.getElementById("setCardName").value = "";
-		//le quito el diseño de focus
-		inputStart.classList.remove('input-focus');
-		inputDiv.className = "list-div"
-		inputDiv.removeChild(btnSave);
-		inputDiv.removeChild(cancel);
+		inputStart.focus();
+		// //le quito el diseño de focus
+		// inputStart.classList.remove("input-focus");
+		// inputDiv.className = "list-div"
+		// inputDiv.removeChild(btnSave);
+		// inputDiv.removeChild(cancel);
+	
 
-		/////////// Evento Input para añadir tarjeta
-		addCardLink.addEventListener('click', function(){
-			var textArea = document.createElement("textarea");
+		/////////// Event Input para añadir tarjeta
+		addCardLink.addEventListener("click", function(){
+			var textAreaCard = document.createElement("textarea");
+			textAreaCard.id = "text"
 			var btnSend = document.createElement("button");
+			btnSend.className = "btn-save";
 			var btnSendText = document.createTextNode("Añadir")
 			btnSend.appendChild(btnSendText);
-
-
-			listDiv.appendChild(textArea);
+			listDiv.appendChild(textAreaCard);
 			listDiv.appendChild(btnSend);
-			//remueve a
+			//remueve añadir tarjeta
 			listDiv.removeChild(addCardLink);
+			//focus textarea
+			textAreaCard.focus();
 
-		})
-		//////////////
+
+			//////////////////////////////////////////////////////////////////////////
+			//me sale este error
+			//Uncaught DOMException: Failed to execute 'removeChild' 
+			//on 'Node': The node to be removed is not a child of this node
+			//no entiendo que significa, pero funciona c: aun así lo dejé comentado
+			//////////////////////// inputStart.addEventListener("focus", abortCard);
+			//////////////////////////////////////////////////////////////////////////
+
+
+			//llamo al input
+			var element = document.getElementById('text');
+			//le agrego un evento que llame a la función resize
+			element.addEventListener('keydown', autosize);
+			//Función que cambia el tamaño del input
+			function autosize(){
+		  		setTimeout(function(){
+			  		//le doy alto inline (dentro del html)
+			  		element.style.cssText = 'height:auto; padding:0';
+			  		//hago que el alto cambie según el alto del contenido del input (scrollHeight)
+					element.style.cssText = 'height:' + element.scrollHeight + 'px';
+	  			},0);
+			}
+
+			btnSend.addEventListener("click", function(){
+				var paragraph = document.createElement("p");
+				var textCard = document.createTextNode(textAreaCard.value);
+				if (textCard.length >0) {
+					paragraph.appendChild(textCard);
+					listDiv.insertBefore(paragraph, textAreaCard);
+					textAreaCard.value = "";
+					//reseteo el alto del input
+					document.getElementById("text").style.cssText = "height: auto;";
+					textAreaCard.focus();
+				} else {
+					alert("Parece que esta tarjeta está vacía. Escribe algo para añadir c:")
+				}
+			});
+
+			////// Cancel Events
+			///creo un X por lista
+			var cancelCard = document.createElement("i");
+			cancelCard.classList.add("fa", "fa-times");
+			listDiv.appendChild(cancelCard)
+
+			cancelCard.addEventListener("click", abortCard);
+			
+			function abortCard(){
+				listDiv.removeChild(textAreaCard);
+				listDiv.appendChild(addCardLink);
+				listDiv.removeChild(btnSend);
+				listDiv.removeChild(cancelCard);
+			};
+		});
+	} else {
+		alert(":o parece que intentas crear una lista está vacía, n ose puede trabajar así!");
 	}
 });
 
 
-
-///////////////////////////////////////////
-
 ///////////////////////////////////////////
 
 //Evento en el input
-inputStart.addEventListener('focus', firstCardFocus);
+inputStart.addEventListener("focus", firstCardFocus);
 function firstCardFocus(){
 	//al hacer click en el input
-	inputStart.classList.add('input-focus');
+	inputStart.classList.add("input-focus");
 	inputDiv.className = "list-div-active"
 	inputDiv.appendChild(btnSave);
 	inputDiv.appendChild(cancel);
 
 }
-cancel.addEventListener('click', firstCardBlur);
+cancel.addEventListener("click", firstCardBlur);
 function firstCardBlur(){
 	//al hacer click en el input
-	inputStart.classList.remove('input-focus');
+	inputStart.classList.remove("input-focus");
 	inputDiv.className = "list-div"
 	inputDiv.removeChild(btnSave);
 	inputDiv.removeChild(cancel);
